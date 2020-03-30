7e101facd522c99c4dd73a37be1a84d4446291c4
cfwu <cfwu@vova.com.hk>
2020-03-30 22:53:42 +0800

    modify

--- src/pages/index/app.js
+++ src/pages/index/app.js
@@ -7,7 +7,7 @@ import React, { Component } from "react";
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <div className="column">
         Hello world
       </div>
     );
